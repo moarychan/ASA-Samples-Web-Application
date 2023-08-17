87e2f42c18824f7657754c6628fe52d45a978f98
Shili Chen <v-shilichen@microsoft.com>
2023-08-17 17:01:15 +0800

    debug ps1

---
+++ infra/azuredeploy/check-provision-state.ps1
@@ -0,0 +1,21 @@
+# Set the API endpoint URL
+$apiUrl = "https://api.example.com/endpoint"
+
+$apiUrl = 'https://management.azure.com/subscriptions/$env:SUBSCRIPTION_ID/resourceGroups/$env:RESOURCE_GROUP/providers/Microsoft.AppPlatform/Spring/$env:ASA_SERVICE_NAME/apps/simple-todo-web/getResourceUploadUrl?api-version=2023-05-01-preview'
+$state = $null
+$timeout = New-TimeSpan -Seconds 180
+$sw = [System.Diagnostics.Stopwatch]::StartNew()
+$accessToken = (Get-AzAccessToken).Token
+while ($sw.Elapsed -lt $timeout) {
+    $headers = @{
+        'Authorization' = 'Bearer ' + $accessToken
+    }
+    $response = Invoke-WebRequest -Uri $apiUrl -Headers $headers -Method GET
+    $state = $response | jq '.properties.provisioningState'
+    if ($state -eq 'Succeeded') {
+        break
+    }
+    Start-Sleep -Seconds 3
+}
+
+Write-Output "State: $state"
